78841fa921037bf4d9881bd6b08c857068639ac9
Martin Kachev <martinbkachev@gmail.com>
2019-10-28 20:20:35 +0100

    init backend commit

---
+++ backend/src/main/java/amos/group3/gitmodconfig_backend/GitmodconfigBackendApplication.java
@@ -0,0 +1,13 @@
+package amos.group3.gitmodconfig_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmodconfigBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitmodconfigBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/amos/group3/gitmodconfig_backend/GitmodconfigBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package amos.group3.gitmodconfig_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmodconfigBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
